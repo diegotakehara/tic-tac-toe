dbe4d9dc1fb4d12733e3f898fb8bc5debafc8050
diegotakehara <diego.takehara@gmail.com>
2023-04-27 09:57:02 -0300

    criacao de icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './Header.module.css'
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
 import Subfooter from "../subfooter/Subfooter"
+import Icon from '../icon/Icon'
 
 function Header ({children}) {
   return (
@@ -10,6 +11,9 @@ function Header ({children}) {
       <Title>Tic-Tac-Toe</Title>
       <Subtitle>(Jogo da Velha)</Subtitle>
       <Subfooter>criado por Diego Takehara</Subfooter>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" /*size="100px" - para quando quiser trocar o tamanho default*/ link="http://github.com/diegotakehara" />
+      </div>
     </div>
   )
 }
