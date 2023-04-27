f3f65cda6b3e77a3a99bd421acdc66c2af88c025
diegotakehara <diego.takehara@gmail.com>
2023-04-27 09:00:39 -0300

    finalizacao do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
+import styles from './Header.module.css'
+
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
 import Subfooter from "../subfooter/Subfooter"
 
 function Header ({children}) {
   return (
-    <div className="container">
+    <div className={styles.header}>
       <Title>Tic-Tac-Toe</Title>
       <Subtitle>(Jogo da Velha)</Subtitle>
       <Subfooter>criado por Diego Takehara</Subfooter>
