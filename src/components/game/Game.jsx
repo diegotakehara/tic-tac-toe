151412ed46d463cda02fc9fca4c1ea200e4d845d
diegotakehara <diego.takehara@gmail.com>
2023-04-27 11:23:38 -0300

    criacao de estados no game option

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react'
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 
+/* let array = Array(9).fill(0) - passamos nessa funcao com qtd de posicoes e em fill o valor em q a 1ª pos começará, mas como react nao pode usar let, passaremos uma const dentro da função:*/
+
 function Game () {
+  const [gameState, setGameState] = useState(Array(9).fill(0))
+
   return (
     <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />      
+      {
+        gameState.map((value, pos)=>
+          <GameOption 
+            key={`game-option-pos-${pos}`}
+          />
+        )
+      } 
     </div>
   )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,13 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
+{/*uma solução para muitos estados(por ex: sizes), para nao trocar um por um, criar um componente atraves de const q retorne essas opçoes dentro do jsx que nao sera exportado para outros lugares*/}
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="27px" />
 
-function GameOption () {
+function GameOption ({ status }) {
   return (
     <div className={styles.gameOption}>
-      
+      { status === 1 && <Icon iconName="circle" /> }
+      { status === -1 && <Icon iconName="x" /> } 
     </div>
   )
 }
